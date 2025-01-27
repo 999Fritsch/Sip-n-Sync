6d9ebe88d8a2e70060d587567189ae4f55771442
Filip Frederik Fritsch <filip.fritsch@gmail.com>
2025-01-27 15:50:29 +0100

    Refactor LoginScreen to use a horizontal layout and display a success message upon login

--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 import sys
 import sqlite3
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLineEdit, QMessageBox, QWidget, QPushButton, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QMessageBox, QWidget, QPushButton, QGridLayout, QHBoxLayout, QLabel
+from PyQt6.QtCore import Qt
 
 class Numpad(QWidget):
     def __init__(self, input_field, enter_callback):
@@ -46,21 +47,19 @@ class LoginScreen(QMainWindow):
 
     def init_ui(self):
         self.setWindowTitle('Login')
-        self.setGeometry(100, 100, 300, 400)
+        self.showFullScreen()
 
         self.textbox_user = QLineEdit(self)
         self.textbox_user.setPlaceholderText('Enter ID')
-        self.textbox_user.setGeometry(50, 50, 200, 40)
         self.textbox_user.setMaxLength(8)
         self.textbox_user.textChanged.connect(self.on_text_changed)
 
         self.numpad = Numpad(self.textbox_user, self.check_login)
-        self.numpad.setGeometry(50, 100, 200, 200)
 
         central_widget = QWidget()
-        layout = QVBoxLayout(central_widget)
-        layout.addWidget(self.textbox_user)
+        layout = QHBoxLayout(central_widget)
         layout.addWidget(self.numpad)
+        layout.addWidget(self.textbox_user)
         self.setCentralWidget(central_widget)
 
     def on_text_changed(self):
@@ -77,9 +76,21 @@ class LoginScreen(QMainWindow):
         conn.close()
 
         if user:
-            QMessageBox.information(self, 'Success', 'Login Successful')
+            self.show_success_message()
         else:
             QMessageBox.warning(self, 'Error', 'Invalid ID')
+
+    def show_success_message(self):
+        # Clear the layout
+        for i in reversed(range(self.layout().count())): 
+            widget = self.layout().itemAt(i).widget()
+            if widget is not None: 
+                widget.deleteLater()
+
+        # Add success message
+        success_label = QLabel('Login Successful', self)
+        success_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout().addWidget(success_label)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
